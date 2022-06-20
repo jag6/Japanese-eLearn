e71111a38c8ed9b58ae3505ad145ee2a01ca8bf3
Matt <matt@randomforestweb.com>
2022-06-21 00:50:56 +0900

    first

---
+++ touch.py
@@ -0,0 +1,19 @@
+import random as r
+from random import sample as s
+
+endure = ["耐える", "たえる"]
+make_new = ["新しくする", "あたらしくする"]
+smile = ["笑う", "わらうあ"]
+
+words = [endure, make_new]
+words = s(words, 1)
+key = words[0][0]
+value = words[0][1]
+
+print('What is', key, '?')
+x = input()
+if x == value:
+    print('Correct')
+else:
+    print('Wrong')
+    
