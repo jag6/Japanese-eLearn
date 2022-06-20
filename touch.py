47df3ad44a68a1cdce8876925ae3b83d0d143593
Matt <matt@randomforestweb.com>
2022-06-21 00:56:11 +0900

    while

--- touch.py
+++ touch.py
@@ -5,15 +5,15 @@ endure = ["耐える", "たえる"]
 make_new = ["新しくする", "あたらしくする"]
 smile = ["笑う", "わらうあ"]
 
-words = [endure, make_new, smile]
-words = s(words, 1)
-key = words[0][0]
-value = words[0][1]
-
-print('What is', key, '?')
-x = input()
-if x == value:
-    print('Correct')
-else:
-    print('Wrong')
-    
+while True:
+    words = [endure, make_new, smile]
+    words = s(words, 1)
+    key = words[0][0]
+    value = words[0][1]
+    print('What is', key, '?')
+    x = input()
+    if x == value:
+        print('Correct')
+    else:
+        print('Wrong')
+        
