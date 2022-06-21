4c8982e2015fe68f8402dded6e195e7afca85beb
Matt <matt@randomforestweb.com>
2022-06-22 00:55:11 +0900

    words

--- touch.py
+++ touch.py
@@ -1,12 +1,6 @@
 import random as r
 from random import sample as s
 
-endure = ["耐える", "たえる"]
-be_apart = ["離れる", "はなれる"]
-smile = ["笑う", "わらう"]
-maintain = ["保つ", "たもつ"]
-praise = ["褒める", "ほめる"]
-
 def commendation():
     commendation = ['正しい！', 'はい、正解です！', '凄い！', '頭が良い！']
     commendation = s(commendation, 1)
@@ -14,10 +8,14 @@ def commendation():
         print(i)
 
 while True:
-    words = [endure, be_apart, smile, maintain, praise]
+    words = [[['endure', '耐える', 'たえる']],
+            [['be_apart', '離れる', 'はなれる']],
+            [['smile', '笑う', 'わらう']],
+            [['maintain', '保つ', 'たもつ']],
+            [['praise', '褒める', 'ほめる']]]
     words = s(words, 1)
-    key = words[0][0]
-    value = words[0][1]
+    key = words[0][0][1]
+    value = words[0][0][2]
     print('What is', key, '?')
     x = input()
     if x == value:
--- touch2.py
+++ touch2.py
@@ -1,12 +1,6 @@
 import random as r
 from random import sample as s
 
-endure = ["耐える", "たえる"]
-be_apart = ["離れる", "はなれる"]
-smile = ["笑う", "わらう"]
-maintain = ["保つ", "たもつ"]
-praise = ["褒める", "ほめる"]
-
 def commendation():
     commendation = ['正しい！', 'はい、正解です！', '凄い！', '頭が良い！']
     commendation = s(commendation, 1)
@@ -14,12 +8,16 @@ def commendation():
         print(i)
 
 while True:
-    words = [endure, be_apart, smile, maintain, praise]
+    words = [[['endure', '耐える', 'たえる']],
+            [['be_apart', '離れる', 'はなれる']],
+            [['smile', '笑う', 'わらう']],
+            [['maintain', '保つ', 'たもつ']],
+            [['praise', '褒める', 'ほめる']]]
     while words:
         r.shuffle(words)
         word = words.pop(0)
-        key = word[0]
-        value = word[1]
+        key = word[0][1]
+        value = word[0][2]
         print('What is', key, '?')
         x = input()
         if x == value:
