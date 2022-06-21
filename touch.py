f23abc467b4a68b8520e1afa329ddf37a9d13c67
Matt <matt@randomforestweb.com>
2022-06-21 18:17:46 +0900

    褒める

--- touch.py
+++ touch.py
@@ -2,18 +2,27 @@ import random as r
 from random import sample as s
 
 endure = ["耐える", "たえる"]
-make_new = ["新しくする", "あたらしくする"]
+be_apart = ["離れる", "はなれる"]
 smile = ["笑う", "わらう"]
+maintain = ["保つ", "たもつ"]
+
+def commendation():
+    commendation = ['正しい！', 'はい、正解です！', '凄い！', '頭が良い！']
+    commendation = s(commendation, 1)
+    for i in commendation:
+        print(i)
 
 while True:
-    words = [endure, make_new, smile]
+    words = [endure, be_apart, smile, maintain]
     words = s(words, 1)
     key = words[0][0]
     value = words[0][1]
     print('What is', key, '?')
     x = input()
     if x == value:
-        print('Correct')
+        commendation()
+        print('Now use', key, 'in a sentence')
+        x = input()
+        commendation()
     else:
         print('Wrong')
-        
